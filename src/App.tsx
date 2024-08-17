d0dcef07f2ed321c31662dc4dfd69b4dc5d61f1f
karan <karanbengani14@gmail.com>
2024-08-17 16:37:14 +0530

    initial configs

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex justify-center'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo h-full w-full" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react h-full w-full" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
